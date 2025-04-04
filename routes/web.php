f71e91b884e989fbe6e944bbdae4d4dff3473e92
bharatdigihub <bk.digitalservice@gmail.com>
2025-04-04 18:14:19 +0530

    Resolved merge conflicts from design-dev

--- routes/web.php
+++ routes/web.php
@@ -24,40 +24,21 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('/Dashboard/Dashboard'); // Ensure this matches the frontend component
     })->name('dashboard');
 
-    // User management routes (restricted to superadmin)
-    Route::middleware([\App\Http\Middleware\SuperAdminMiddleware::class])->group(function () {
-        // User routes
-        Route::get('/users', [UserController::class, 'index'])->name('users.index'); // Add this line for listing users
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-        Route::post('/users', [UserController::class, 'store'])->name('users.store');
-        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::patch('/users/{user}', [UserController::class, 'update'])->name('users.update');
-        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    // User routes
+    Route::get('/users', [UserController::class, 'index'])->name('users.index'); // Add this line for listing users
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::patch('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
-        // Role routes
-        Route::get('/roles', [RoleController::class, 'index'])->name('roles.index'); // List roles
-        Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create'); // Add role form
-        Route::post('/roles', [RoleController::class, 'store'])->name('roles.store'); // Store role
-        Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit'); // Edit role form
-        Route::patch('/roles/{role}', [RoleController::class, 'update'])->name('roles.update'); // Update role
-        Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy'); // Delete role
-
-        // Permission routes
-        Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions.index'); // List permissions
-        Route::get('/permissions/create', [PermissionController::class, 'create'])->name('permissions.create'); // Add permission form
-        Route::post('/permissions', [PermissionController::class, 'store'])->name('permissions.store'); // Store permission
-        Route::get('/permissions/{permission}/edit', [PermissionController::class, 'edit'])->name('permissions.edit'); // Edit permission form
-        Route::patch('/permissions/{permission}', [PermissionController::class, 'update'])->name('permissions.update'); // Update permission
-        Route::delete('/permissions/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy'); // Delete permission
-
-        // Menu routes
-        Route::get('/menus', [\App\Http\Controllers\MenuController::class, 'index'])->name('menus.index'); // List menus
-        Route::get('/menus/create', [\App\Http\Controllers\MenuController::class, 'create'])->name('menus.create'); // Add menu form
-        Route::post('/menus', [\App\Http\Controllers\MenuController::class, 'store'])->name('menus.store'); // Store menu
-        Route::get('/menus/{menu}/edit', [\App\Http\Controllers\MenuController::class, 'edit'])->name('menus.edit'); // Edit menu form
-        Route::patch('/menus/{menu}', [\App\Http\Controllers\MenuController::class, 'update'])->name('menus.update'); // Update menu
-        Route::delete('/menus/{menu}', [\App\Http\Controllers\MenuController::class, 'destroy'])->name('menus.destroy'); // Delete menu
-    });
+    // Menu routes
+    Route::get('/menus', [\App\Http\Controllers\MenuController::class, 'index'])->name('menus.index'); // List menus
+    Route::get('/menus/create', [\App\Http\Controllers\MenuController::class, 'create'])->name('menus.create'); // Add menu form
+    Route::post('/menus', [\App\Http\Controllers\MenuController::class, 'store'])->name('menus.store'); // Store menu
+    Route::get('/menus/{menu}/edit', [\App\Http\Controllers\MenuController::class, 'edit'])->name('menus.edit'); // Edit menu form
+    Route::patch('/menus/{menu}', [\App\Http\Controllers\MenuController::class, 'update'])->name('menus.update'); // Update menu
+    Route::delete('/menus/{menu}', [\App\Http\Controllers\MenuController::class, 'destroy'])->name('menus.destroy'); // Delete menu
 
     // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
